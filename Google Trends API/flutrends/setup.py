1f70dd2e938764bd730e3b0c51c0a9503abe021f
Karla Mejia <kmejia@hsph.harvard.edu>
2018-09-14 09:05:54 -0400

    trends

---
+++ Google Trends API/flutrends/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flutrends',
+      version='1.0',
+      description='Get data from Google Flu Trends API',
+      url='http://github.com/karlamejia/Projects',
+      author='Karla Mejia',
+      author_email='mejiakarla10@gmail.com',
+      license='MIT',
+      packages=['flutrends'],
+      zip_safe=False)
